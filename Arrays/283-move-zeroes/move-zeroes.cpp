e9c064ef6f606671849d79b64059cb8fefc65424
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-02 11:21:04 +0530

    Time: 12 ms (99.04%) | Memory: 19.1 MB (69.53%) - LeetSync

--- Arrays/283-move-zeroes/move-zeroes.cpp
+++ Arrays/283-move-zeroes/move-zeroes.cpp
@@ -1,23 +1,40 @@
-class Solution {
+class Solution 
+{
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(vector<int>& nums) 
+    {
         
-        vector<int> a;
-        int count=0;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]==0)
-            count++;
-        else
-            a.push_back(nums[i]);
-    }
-    
-    while(count--)
-        a.push_back(0);
-    for(int i=0;i<nums.size();i++){
-        nums[i]=a[i];
-    } 
+        //     vector<int> a;
+        //     int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0)
+        //         count++;
+        //     else
+        //         a.push_back(nums[i]);
+        // }
+        
+        // while(count--)
+        //     a.push_back(0);
+        // for(int i=0;i<nums.size();i++){
+        //     nums[i]=a[i];
+        // } 
 
+        // optimal solution 
+        // Dutch National Flag Algo....
 
+            int j=0,count=0, n=nums.size();
+        while(count<n){
+         if(nums[count]==0)
+              count++;
+         else{
+              int temp=nums[j];
+              nums[j]=nums[count];
+              nums[count]=temp;
+              j++;
+              count++;
+            }
+        }
+}
     
-    }
+    
 };
