1fda302806bfc0bebdc6d06394f356645c5b3b5c
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-06 11:20:30 +0530

    Time: 13 ms (74.49%) | Memory: 16.2 MB (71.31%) - LeetSync

--- Arrays/2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ Arrays/2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans ;
-        for(int i=0; i<nums.size(); i++){
-            ans.push_back(nums[nums[i]]);
-        }
-        return ans;
-     }
+    //     vector<int> ans ;
+    //     for(int i=0; i<nums.size(); i++){
+    //         ans.push_back(nums[nums[i]]);
+    //     }
+    //     return ans;
+    //  }
+
+    // optimized solution with o(n) complexity.....
+    int n = nums.size();
+    
+    for(int i=0; i<n; i++){
+        nums[i] = n * (nums[nums[i]]%n) + nums[i];
+
+    }
+
+    for(int i=0; i<n; i++){
+        nums[i] = nums[i] / n;
+    }
+
+    return nums;
+    }
 };
