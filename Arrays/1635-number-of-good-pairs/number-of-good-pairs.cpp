bf945ed8f7d86e6dcde382fcb5c0a9a8e20ecc2d
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-06 12:16:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (26.77%) - LeetSync

--- Arrays/1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ Arrays/1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -2,15 +2,33 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         
+        // Navie solution
+        // int count = 0;
+        // for(int i=0;i<nums.size()-1;i++)
+        // {
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //         if(i<j && nums[i]==nums[j])
+        //             count++;
+        //     }
+        // }
+        // return count;
+
+        // optimized with hashing 
+
         int count = 0;
-        for(int i=0;i<nums.size()-1;i++)
+        unordered_map<int,int>ans;
+        for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(i<j && nums[i]==nums[j])
-                    count++;
-            }
+            ans[nums[i]]++;
+        }
+        
+        for(auto it: ans)
+        {
+            int val = it.second;
+            count = count + (val*(val-1))/2;
         }
         return count;
+
     }
 };
