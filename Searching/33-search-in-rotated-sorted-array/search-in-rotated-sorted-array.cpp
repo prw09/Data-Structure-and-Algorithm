76694e8294a20c80881816b2ab6774e72527a7a1
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-14 15:35:09 +0530

    Time: 3 ms (70.70%) | Memory: 11.1 MB (18.44%) - LeetSync

--- Searching/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ Searching/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,13 +3,37 @@ public:
     int search(vector<int>& nums, int target) {
         
         // Linear Search Approach ...
-        int n = nums.size() , x = target;
-        for(int i=0; i<n; i++){
-            if(nums[i] == x){
-                return i;
-            }
-        }
+        // int n = nums.size() , x = target;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] == x){
+        //         return i;
+        //     }
+        // }
 
-        return -1;
+        // return -1;
+
+        // Binary Search Approach.....
+
+    int l = 0;
+    int r = nums.size() - 1;
+
+    while (l <= r) {
+      const int m = (l + r) / 2;
+      if (nums[m] == target)
+        return m;
+      if (nums[l] <= nums[m]) {  // nums[l..m] are sorted
+        if (nums[l] <= target && target < nums[m])
+          r = m - 1;
+        else
+          l = m + 1;
+      } else {  // nums[m..n - 1] are sorted
+        if (nums[m] < target && target <= nums[r])
+          l = m + 1;
+        else
+          r = m - 1;
+      }
+    }
+
+    return -1;
     }
 };
