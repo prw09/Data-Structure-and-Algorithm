2ee73acd72724c43190a54534a21b097cab098d3
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-15 11:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (72.04%) - LeetSync

--- Searching/2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ Searching/2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,38 +1,60 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-       vector<int> indices;
-        sort(nums.begin(),nums.end());
+       
+    //    Binary search approach.....
+    //    vector<int> indices;
+        // sort(nums.begin(),nums.end());
 
-        int lb=0,ub=(int)nums.size()-1;
-        int mid;
+        // int lb=0,ub=(int)nums.size()-1;
+        // int mid;
 
-        while(lb<=ub)
-        {
-            mid=lb+(ub-lb)/2;
-            if(nums[mid]==target)
-            {
-                indices.push_back(mid);
-                break;
-            }
-            else if(nums[mid]>target)
-            {
-                ub=mid-1;
-            }
-            else lb=mid+1;
-        }
+        // while(lb<=ub)
+        // {
+        //     mid=lb+(ub-lb)/2;
+        //     if(nums[mid]==target)
+        //     {
+        //         indices.push_back(mid);
+        //         break;
+        //     }
+        //     else if(nums[mid]>target)
+        //     {
+        //         ub=mid-1;
+        //     }
+        //     else lb=mid+1;
+        // }
 
-        lb = mid-1;
-        while(lb >= 0 && nums[lb] == target){
-            indices.insert(indices.begin(), lb);
-            lb--;
+        // lb = mid-1;
+        // while(lb >= 0 && nums[lb] == target){
+        //     indices.insert(indices.begin(), lb);
+        //     lb--;
+        // }
+        
+        // ub = mid+1;
+        // while(ub <= (int)(nums.size()-1) && nums[ub] == target){
+        //     indices.push_back(ub);
+        //     ub++;
+        // }
+
+        //  without sorting ...
+
+        vector<int> ans;
+        
+        int index = 0;
+        int frequency = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] < target)
+                index++;
+            else if(nums[i] == target)
+                frequency++;
         }
         
-        ub = mid+1;
-        while(ub <= (int)(nums.size()-1) && nums[ub] == target){
-            indices.push_back(ub);
-            ub++;
+        while(frequency > 0){
+            ans.push_back(index);
+            frequency--;
+            index++;
         }
-        return indices;
+        return ans;
     }
 };
