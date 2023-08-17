92e85e6b812396a987b6aa1073cf7a41ee78c024
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-17 14:13:37 +0530

    Time: 11 ms (92.34%) | Memory: 17.7 MB (18.57%) - LeetSync

--- Searching/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ Searching/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int cntpositive = 0, cntnegative = 0 , n = nums.size();
+     //   int cntpositive = 0, cntnegative = 0 , n = nums.size();
        
-      // traverse the bnumsay to count
-      // occurrences of positive and negative numbers
-      for(int i = 0; i < n; i++) {
-          if( nums[i] > 0 )
-              cntpositive++;
-          else if( nums[i] < 0 )
-              cntnegative++;
-    }
+      // traverse the nums to count
+    //   // occurrences of positive and negative numbers
+    //   for(int i = 0; i < n; i++) {
+    //       if( nums[i] > 0 )
+    //           cntpositive++;
+    //       else if( nums[i] < 0 )
+    //           cntnegative++;
+    // }
    
-      // return maximum among positive
-      // and negative count
-      return max(cntpositive, cntnegative);
+    //   // return maximum among positive
+    //   // and negative count
+    //   return max(cntpositive, cntnegative);
+
+    // Binary Search ....
+
+        // int n = nums.size();
+        // int low = 0 , mid , high = n - 1;
+
+        // while(low<=high){
+
+        // mid = low + (high - low) / 2;
+
+        // if(nums[mid] > 0)
+        //     low = mid + 1;
+
+        // else if(nums[mid] < 0)
+        //     high = mid - 1;
+        // }
+        // return (low , n - 1); 
+
+        // best approach
+        int neg = 0, pos = 0;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] < 0) {
+                neg++;
+            } else if(nums[i] > 0){
+                pos++;
+            }
+        }
+        return pos > neg ? pos: neg;
+
 
     }
 };
