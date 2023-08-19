1106deac5dc636ab08a283ea1ef938433c7b4c51
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-19 10:41:54 +0530

    Time: 20 ms (80.41%) | Memory: 22.3 MB (77.96%) - LeetSync

--- Searching/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ Searching/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -31,21 +31,53 @@ public:
     // return -1;
 
 
-        if(n == 0) {
-            return nums[0];
-            }
-        if(n == 1) {
-            return nums[0];
-        }
-            int i = 0;
-            while(i < n-1) {
-            if(nums[i] != nums[i+1]) {
-            return nums[i];
-            }
-            i = i + 2;
-            }
-            return nums[i];
+        // if(n == 0) {
+        //     return nums[0];
+        //     }
+        // if(n == 1) {
+        //     return nums[0];
+        // }
+        //     int i = 0;
+        //     while(i < n-1) {
+        //     if(nums[i] != nums[i+1]) {
+        //     return nums[i];
+        //     }
+        //     i = i + 2;
+        //     }
+        //     return nums[i];
 
-   
+
+        // Binary search...
+
+        // Edge cases:
+    if (n == 1) return nums[0];
+    if (nums[0] != nums[1]) return nums[0];
+    if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+    int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if nums[mid] is the single element:
+        if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+            return nums[mid];
+        }
+
+        //we are in the left:
+        if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
+                || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+            //eliminate the left half:
+            low = mid + 1;
+        }
+        //we are in the right:
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
+    }
+
+    // dummy return statement:
+    return -1;
+
     }
 };
