d9848f4e9da3f2c71dd206e6f6ae83549873d724
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-25 22:10:00 +0530

    Time: 292 ms (36.63%) | Memory: 10.1 MB (76.12%) - LeetSync

---
+++ Hashing/1-two-sum/two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       
+       int onept = 0, twopt = 0 , n = nums.size();
+
+       for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1; j<n;j++)
+            {
+                
+                // if(nums[i] == nums[j]) continue;
+                if(nums[i]+nums[j]==target)
+                {
+                    onept=i;
+                    twopt=j;
+                }
+            }
+        }
+        return {onept,twopt};  
+    }  
+};
