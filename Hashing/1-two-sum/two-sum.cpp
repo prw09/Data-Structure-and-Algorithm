83d74592cbf8d1a527c646600aeca10ba3c23549
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2023-08-25 22:19:50 +0530

    Time: 3 ms (99.51%) | Memory: 10.8 MB (44.45%) - LeetSync

--- Hashing/1-two-sum/two-sum.cpp
+++ Hashing/1-two-sum/two-sum.cpp
@@ -2,21 +2,38 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
        
-       int onept = 0, twopt = 0 , n = nums.size();
+    //    int onept = 0, twopt = 0;
+       int n = nums.size();
 
-       for(int i=0;i<n-1;i++)
-        {
-            for(int j=i+1; j<n;j++)
-            {
+    //    for(int i=0;i<n-1;i++)
+    //     {
+    //         for(int j=i+1; j<n;j++)
+    //         {
                 
-                // if(nums[i] == nums[j]) continue;
-                if(nums[i]+nums[j]==target)
-                {
-                    onept=i;
-                    twopt=j;
-                }
-            }
+    //             // if(nums[i] == nums[j]) continue;
+    //             if(nums[i]+nums[j]==target)
+    //             {
+    //                 onept=i;
+    //                 twopt=j;
+    //             }
+    //         }
+    //     }
+    //     return {onept,twopt};  
+
+
+            // Hashing Approch...
+
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = nums[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
         }
-        return {onept,twopt};  
+        mpp[num] = i;
+    }
+    return { -1, -1};
+
+
     }  
 };
