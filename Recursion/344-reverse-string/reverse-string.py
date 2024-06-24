73761d6325e6951c06e9409b55d72f62b51e794d
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2024-06-24 21:03:19 +0530

    Time: 172 ms (35.24%) | Memory: 20.8 MB (49.38%) - LeetSync

--- Recursion/344-reverse-string/reverse-string.py
+++ Recursion/344-reverse-string/reverse-string.py
@@ -1,9 +1,31 @@
+# class Solution:
+#   def reverseString(self, s: List[str]) -> None:
+#     # l = 0
+#     # r = len(s) - 1
+
+#     # while l < r:
+#     #   s[l], s[r] = s[r], s[l]
+#     #   l += 1
+#     #   r -= 1
+
+#    a = len(s) - 1
+#    result = ''
+  
+    
+#    while a>= 0:
+#     result = result + s[a]
+#     result = result - 1 
+
+
 class Solution:
   def reverseString(self, s: List[str]) -> None:
-    l = 0
-    r = len(s) - 1
-
-    while l < r:
-      s[l], s[r] = s[r], s[l]
-      l += 1
-      r -= 1
+    a = len(s) - 1
+    result = ''
+    
+    while a >= 0:
+      result = result + s[a]
+      a -= 1
+      
+    # Since we need to modify the list in place, we need to update the original list 's'
+    for i in range(len(s)):
+      s[i] = result[i]
