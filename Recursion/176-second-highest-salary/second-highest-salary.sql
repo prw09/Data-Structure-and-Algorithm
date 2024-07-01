336a21c4ca843d2625d27af5dd5eb9b5f887cb0c
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2024-07-01 12:01:12 +0530

    Time: 238 ms (81.49%) | Memory: 0B (100.00%) - LeetSync

--- Recursion/176-second-highest-salary/second-highest-salary.sql
+++ Recursion/176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,13 @@
+-- SELECT MAX(salary) AS SecondHighestSalary
+-- FROM Employee
+-- WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+
+WITH
+  RankedEmployees AS (
+    SELECT *, DENSE_RANK() OVER(ORDER BY salary DESC) AS `rank`
+    FROM Employee
+  )
 SELECT MAX(salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee);
+FROM RankedEmployees
+WHERE `rank` = 2;
