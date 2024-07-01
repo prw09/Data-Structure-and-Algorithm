442e0c419dd0409273feeb16cf5f142e3f5ad36f
Piyush Wandile <98741813+prw09@users.noreply.github.com>
2024-07-01 16:02:03 +0530

    Time: 467 ms (64.26%) | Memory: 0B (100.00%) - LeetSync

--- Recursion/1258-article-views-i/article-views-i.sql
+++ Recursion/1258-article-views-i/article-views-i.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
--- select distinct(author_id) as id 
--- from Views
--- where author_id = viewer_id
--- order by id 
+select distinct(author_id) as id 
+from Views
+where author_id = viewer_id
+order by id 
 
 
-SELECT DISTINCT author_id AS id
-FROM Views
-WHERE author_id = viewer_id
-ORDER BY 1;
+-- SELECT DISTINCT author_id AS id
+-- FROM Views
+-- WHERE author_id = viewer_id
+-- ORDER BY 1;
